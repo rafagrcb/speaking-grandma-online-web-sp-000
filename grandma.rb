2953405a4a0d93c4074671347c172bbaaf23a9c2
Rafael Garcia <rafagrcb@gmail.com>
2020-05-07 22:35:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(phrase)
    if phrase == "I LOVE YOU GRANDMA!"
-				        "I LOVE YOU TOO PUMPKIN"
-				 elsif phrase != phrase.upcase
-				        "HUH!? SPEAK UP SONNY!"
-				 else phrase == phrase.upcase
-				        "NO, NOT SINCE 1938!"
-				end
+     return "I LOVE YOU TOO PUMPKIN"
+	 elsif phrase != phrase.upcase
+	   return "HUH!? SPEAK UP SONNY!"
+	 else phrase == phrase.upcase
+	   return "NO, NOT SINCE 1938!"
+	 end
 end 

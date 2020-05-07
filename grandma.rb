f7510699063a8d7d8e77314dc8218cd65f71870a
Rafael Garcia <rafagrcb@gmail.com>
2020-05-07 22:30:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,8 +2,8 @@ def speak_to_grandma(phrase)
   if phrase.match(/\p{Lower}/) == nil
     return('NO, NOT SINCE 1938!')
   elsif
-    return("HUH?! SPEAK UP, SONNY!")
+    return "HUH?! SPEAK UP, SONNY!" 
   else phrase == "I LOVE YOU GRADMA!"
-    return("I LOVE YOU TOO PUMPKIN!")
+    return "I LOVE YOU TOO PUMPKIN!"
   end
 end

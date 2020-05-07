299bee4c58c8e9ae0163064ecf0cd9dbb3a7babc
Rafael Garcia <rafagrcb@gmail.com>
2020-05-07 22:15:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,13 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "Hi!" || "Hi Nana, How are you?"
+  elsif phrase == "Hi Nana, How are you?"
     return "HUH?! SPEAK UP, SONNY!"
-  else phrase == "WHAT DID YOU EAT TODAY?" || "WHAT?"
+  elsif phrase == "Hi!"
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "WHAT DID YOU EAT TODAY?"
+    return "NO, NOT SINCE 1938!"
+  else phrase == "WHAT?"
     return "NO, NOT SINCE 1938!"
   end
 end
